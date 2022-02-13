6aec0f1ed84c6d1017683bb556261324dc871344
Laptop My Linh <mylinh2152002@gmail.com>
2022-02-13 08:39:57 +0700

    Init code

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 2105
+
+app.get('/', (req, res) => {
+  res.send('Hello World! Ecommerce Blockchain')
+})
+
+app.listen(port, () => {
+  console.log(`Ecommerce Blockchain app listening on port ${port}`)
+})
